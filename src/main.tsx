d9f521410958afe46b7bdc13915c4a24aace6481
mohamed128450932 <mohamedabdall112985@gmail.com>
2025-06-29 19:18:32 +0100

    hello

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Always allow scroll/pull-to-refresh on mobile
+if (typeof document !== 'undefined') {
+  document.body.style.overflow = 'unset';
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
